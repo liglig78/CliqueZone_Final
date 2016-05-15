99e9bce6b6bda7163ad75bef79e017a7e9dabab0
liglig78 <liglig78@gmail.com>
2016-05-15 20:17:45 +0300

    task to string

--- src/DB/Types/Task.java
+++ src/DB/Types/Task.java
@@ -1,22 +1,126 @@
 package DB.Types;
 
-import java.util.TimeZone;
+import java.util.Objects;
 
 /**
  * Created by ASUS-PC on 13/05/2016.
  */
-public class Task {
+public class Task extends GeneralDBType {
     private int idTask;
     private String address;
     private String telephone;
-    private TimeZone creatintionTime;
+    private String creatintionTime;
     private float level;
     private  boolean toolShed;
     private  boolean vipCustomer;
-    private  TimeZone dueDate;
-    private TimeZone windowToSupply;
-    private TimeZone timeToFix;
+    private  String dueDate;
+    private String windowToSupply;
+    private String timeToFix;
     private String decripation;
 
 
+    @Override
+    public String toString() {
+        return "Task{" +
+                "idTask=" + idTask +
+                ", address='" + address + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", creatintionTime='" + creatintionTime + '\'' +
+                ", level=" + level +
+                ", toolShed=" + toolShed +
+                ", vipCustomer=" + vipCustomer +
+                ", dueDate='" + dueDate + '\'' +
+                ", windowToSupply='" + windowToSupply + '\'' +
+                ", timeToFix='" + timeToFix + '\'' +
+                ", decripation='" + decripation + '\'' +
+                '}';
+    }
+
+    public int getIdTask() {
+        return idTask;
+    }
+
+    public void setIdTask(int idTask) {
+        this.idTask = idTask;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCreatintionTime() {
+        return creatintionTime;
+    }
+
+    public void setCreatintionTime(String creatintionTime) {
+        this.creatintionTime = creatintionTime;
+    }
+
+    public float getLevel() {
+        return level;
+    }
+
+    public void setLevel(float level) {
+        this.level = level;
+    }
+
+    public boolean isToolShed() {
+        return toolShed;
+    }
+
+    public void setToolShed(boolean toolShed) {
+        this.toolShed = toolShed;
+    }
+
+    public boolean isVipCustomer() {
+        return vipCustomer;
+    }
+
+    public void setVipCustomer(boolean vipCustomer) {
+        this.vipCustomer = vipCustomer;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getWindowToSupply() {
+        return windowToSupply;
+    }
+
+    public void setWindowToSupply(String windowToSupply) {
+        this.windowToSupply = windowToSupply;
+    }
+
+    public String getTimeToFix() {
+        return timeToFix;
+    }
+
+    public void setTimeToFix(String timeToFix) {
+        this.timeToFix = timeToFix;
+    }
+
+    public String getDecripation() {
+        return decripation;
+    }
+
+    public void setDecripation(String decripation) {
+        this.decripation = decripation;
+    }
 }
