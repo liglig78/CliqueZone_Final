1511d76b4650a41f5eae128dd1bd030995c54084
ststav <st_stav@live.com>
2016-05-15 20:09:59 +0300

    הצלחנו לקרוא את קובץ הטכנאים!!

--- src/DB/DBActions/ReadExcel.java
+++ src/DB/DBActions/ReadExcel.java
@@ -1,26 +1,62 @@
 package DB.DBActions;
 
 import DB.Types.GeneralDBType;
+import DB.Types.Task;
+import DB.Types.Technician;
 import com.opencsv.CSVReader;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Created by Toshiba on 13/05/2016.
  */
 public class ReadExcel {
 
+   private static List<Technician> techniciansList;
+   private static List<Task> taskList;
+
+    public ReadExcel() {
+        this.techniciansList = new ArrayList<Technician>();
+        this.taskList = new ArrayList<Task>();
+    }
 
     public static void main (String args[]) throws IOException {
         String PROJECT_PATH = new File(".").getCanonicalPath();
         String fileNameAgents = PROJECT_PATH  + "\\src\\resources\\files\\AgentsDB.csv";
 
-        System.out.println(fileNameAgents);
-        readExcelFile(fileNameAgents);
+        readTechniciansExcelFile(fileNameAgents);
+       // readTasksExcelFile(fileNameAgents);
+    }
+
+    private static void readTechniciansExcelFile(String fileName) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader(fileName));
+        String [] nextLine;
+        //id,name,Beginning time,work houres,specialty,HomeLocation,seniority,isInOffice
+
+        reader.readNext();
+
+        while ((nextLine = reader.readNext()) != null) {
+
+            Technician technician = new Technician();
+
+            technician.setId(Integer.parseInt(nextLine[0]));
+            technician.setName(nextLine[1]);
+            technician.setBeginningTime(nextLine[2]);
+            technician.setWorkHoures(Float.parseFloat(nextLine[3]));
+            technician.setSpecialty(Int.parseFloat(nextLine[3]));
+
+
+
+            techniciansList.add(technician);
+
+
+
+            // nextLine[] is an array of values from the line
+            System.out.println(nextLine[0] +", " + nextLine[1] +", " +  nextLine[1]);
+            System.out.println(technician.toString());
+        }
     }
 
     public static List<GeneralDBType> readExcelFile(String fileName) throws IOException {
--- src/DB/Types/Technician.java
+++ src/DB/Types/Technician.java
@@ -1,8 +1,4 @@
 package DB.Types;
-
-import java.sql.Time;
-import java.util.TimeZone;
-
 /**
  * Created by ASUS-PC on 13/05/2016.
  */
@@ -10,11 +6,90 @@ public class Technician {
 
     int id;
     String name;
-    TimeZone beginningTime;
+    String beginningTime;
     float workHoures;
     int specialty;
     String homeLocation;
     int seniority;
     boolean isInOffice;
+
+    @Override
+    public String toString() {
+        return "Technician{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", beginningTime='" + beginningTime + '\'' +
+                ", workHoures=" + workHoures +
+                ", specialty=" + specialty +
+                ", homeLocation='" + homeLocation + '\'' +
+                ", seniority=" + seniority +
+                ", isInOffice=" + isInOffice +
+                '}';
+    }
+
+    public boolean isInOffice() {
+        return isInOffice;
+    }
+
+    public void setInOffice(boolean inOffice) {
+        isInOffice = inOffice;
+    }
+
+    public int getSeniority() {
+        return seniority;
+    }
+
+    public void setSeniority(int seniority) {
+        this.seniority = seniority;
+    }
+
+    public String getHomeLocation() {
+        return homeLocation;
+    }
+
+    public void setHomeLocation(String homeLocation) {
+        this.homeLocation = homeLocation;
+    }
+
+    public int getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(int specialty) {
+        this.specialty = specialty;
+    }
+
+    public float getWorkHoures() {
+        return workHoures;
+    }
+
+    public void setWorkHoures(float workHoures) {
+        this.workHoures = workHoures;
+    }
+
+    public String getBeginningTime() {
+        return beginningTime;
+    }
+
+    public void setBeginningTime(String beginningTime) {
+        this.beginningTime = beginningTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 
 }
