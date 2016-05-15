292e03b723c8330c19a8b393f29d495e6c650dea
Toshiba <st_stav@live.com>
2016-05-15 18:21:53 +0300

    read csv - in hebrew

--- src/DB/DBActions/ReadExcel.java
+++ src/DB/DBActions/ReadExcel.java
@@ -11,20 +11,26 @@ import java.util.List;
  * Created by Toshiba on 13/05/2016.
  */
 public class ReadExcel {
-    private static String fileNameAgents = "C:\Users\Toshiba\Google Drive\המכללה למנהל\שנה שלישית\פרוייקט גמר\כליקשטח\איפיון\DB\תיקיה חדשה\AgentsDB.csv";
+    private static String fileNameAgents = "C:\\Users\\Toshiba\\Google Drive\\המכללה למנהל\\שנה שלישית\\פרוייקט גמר\\כליקשטח\\איפיון\\DB\\תיקיה חדשה\\AgentsDB.csv";
 
     public static void main (String args[]) {
+        try {
+            readExcelFile(fileNameAgents);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public List<GeneralDBType> readExcelFile (String fileName) throws IOException {
+    public static List<GeneralDBType> readExcelFile(String fileName) throws IOException {
         CSVReader reader = new CSVReader(new FileReader(fileName));
 
         String [] nextLine;
 
         while ((nextLine = reader.readNext()) != null) {
             // nextLine[] is an array of values from the line
-            System.out.println(nextLine[0] + nextLine[1] + "etc...");
+            System.out.println(nextLine[0] + nextLine[1] + "סתיו...");
         }
+        return null;
     }
 }
